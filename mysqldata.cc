0963f9bef7165d8dfae0ff29d61ada7d6da76c7e
ParkSeungwon <zezeon1@gmail.com>
2016-07-08 16:17:08 +0900

    b

--- mysqldata.cc
+++ mysqldata.cc
@@ -1,10 +1,9 @@
 #include <chrono>
 #include <ctime>
-#include <unordered_map>
 #include "mysqldata.h"
 using namespace std;
 
-bool QueryData::create_table(const SqlData& d)
+bool SqlData::create_table(const SqlData& d)
 {
 	string q = "create table " + d.table_name + " (";
 	for(auto& a : d.structure) q += " " + a.first + " " + a.second + ", ";
@@ -12,20 +11,21 @@ bool QueryData::create_table(const SqlData& d)
 	return myQuery(q);
 }
 
-bool QueryData::insert(const SqlData& d)
-{
+bool SqlData::insert(const SqlData& d)
+{//d should be 1 record
 	string q = "insert into " + d.table_name + " values (";
-	for(int i = 0; i < d.structure.size(); i++) {
-		if(d.structure[i].second.find("int(") != string::npos)//can find
-			q += d.contents[i] + ",";
-		else q += "'" + d.contents[i] + "',";
+	int i = 0;
+	for(auto& a : d.structure) {
+		if(a.second.find("int(") != string::npos) q += record[i] + ",";//int?
+		else q += "'" + record[i] + "',";
+		i++;
 	}
 	q.back() = ')';
 	q += ";";
 	return myQuery(q);
 }
 
-SqlData QueryData::select(string table, string where)
+SqlData SqlData::select(string table, string where)
 {
 	string q = "select * from " + table;
 	if(where != "") q += " where " + where;
@@ -34,12 +34,10 @@ SqlData QueryData::select(string table, string where)
 
 	SqlData d;
 	d.table_name = table;
-	sql::ResultSetMetaData* res_meta = res->getMetaData();
-	int c = res_meta->getColumnCount();
-	for(int i = 0; i < c; i++) {
-		d.structure.push_back({res_meta->getColumnName(i+1), 
-				res_meta->getColumnTypeName(i+1)});
-	}
+	sql::ResultSetMetaData* mt = res->getMetaData();
+	int c = mt->getColumnCount();
+	for(int i = 0; i < c; i++) 
+		d.structure[mt->getColumnName(i+1)] = mt->getColumnTypeName(i+1);
 	while(res->next()) {
 		for(int i = 0; i < c; i++) {
 			d.contents.push_back(res->getString(i+1));
@@ -48,7 +46,7 @@ SqlData QueryData::select(string table, string where)
 	return d;
 }
 
-string QueryData::now()
+string SqlData::now()
 {
 	auto now = chrono::system_clock::now();
 	auto tp = chrono::system_clock::to_time_t(now);
--- mysqldata.h
+++ mysqldata.h
@@ -1,23 +1,24 @@
 #pragma once
 #include <string>
 #include <vector>
+#include<unordered_map>
 #include "mysqlquery.h"
 
-struct SqlData
-{
-	std::string table_name;
-	std::vector<std::pair<std::string, std::string>> structure;
-	std::string extra;
-	std::string engine;
-	std::vector<std::string> contents;
-};
-
-class QueryData : public Mysqlquery
+class SqlData : public Mysqlquery
 {
 public:
 	bool create_table(const SqlData& data);
 	bool insert(const SqlData& data);
 	SqlData select(std::string table, std::string where = "");
 	static std::string now();//system clock->mysql datetime string
+
+protected:
+	std::string table_name;
+	std::unordered_map<std::string, std::string> structure;
+	std::string extra;
+	std::string engine;
+	std::vector<std::string> record;
+	std::vector<record> contents;
+
 };
 
